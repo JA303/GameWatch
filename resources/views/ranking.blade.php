806c61cf08cf39e6418065cde201a2e10594d509
Ahmad <ahmaderfani12@gmail.com>
2021-02-06 15:36:39 +0330

    Adjust Ranking

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -12,24 +12,24 @@
 
         </div>
         <div class="title-text-holder row"></div>
-        <p class="py-1">You can get karma by get up votes</p>
+        <h5 class="py-2">You can get karma by get up votes</h5>
     </div>
     <!-- Users -->
     <div class="container">
-        <div class="row bg-dark align-middle" style="height: 50px;">
+        <div class="row bg-dark align-middle round-corner-1 shadow-box-1" >
             <div class="col" style="flex: 0 0 75px;">
 
             </div>
             <div class="col-6">
-                <h5 class="text-success">USER NAME</h5>
+                <h5 class="green-text p-2">USER NAME</h5>
             </div>
-            <div class="col">
-                <h5 class="text-warning">KARMA</h5>
+            <div class="col d-flex justify-content-center">
+                <h5 class="text-warning p-2">KARMA</h5>
             </div>
         </div>
         <?php $rank = 1 ?>
         @foreach($users as $user)
-            <div class="row bg-dark" style="height: 40px; margin: 50px 0;">
+            <div class="row bg-dark round-corner-2 rank-holder shadow-box-1" >
                 <div class="col" style="flex: 0 0 75px;">
                     <img  class="no-padding-col rounded-circle commet-row-avatar" style="position: relative; top: -25%; display: inline" src="{{ asset('uploads/avatars/'.$user->avatar) }}">
                     <div style="width: 25px; height: 25px; position: absolute; top: 30px; text-align: center" class="rounded-circle bg-light font-weight-bold" >
@@ -37,12 +37,12 @@
                         {{ $rank++ }}
                     </div>
                 </div>
-                <div class="col-6">
-                    <a href="{{ route('user.profile', $user) }}" class="text-decoration-none">
-                        <h5 class="text-success align-middle">{{{ $user->name }}}</h5>
+                <div class="col-6 pt-2">
+                    <a href="{{ route('user.profile', $user) }}" class="text-decoration-none ">
+                        <h5 class="green-text align-middle ">{{{ $user->name }}}</h5>
                     </a>
-                </div>
-                <div class="col">
+                </div class="">
+                <div class="col d-flex justify-content-center pt-2">
                     <h4 class="text-warning">{{{ $user->karma }}}</h4>
                 </div>
             </div>
